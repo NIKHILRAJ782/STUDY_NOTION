ee15f2d62ca80d7de80c4424e2b40883650cd21f
NIKHILRAJ782 <nikraj782@gmail.com>
2024-05-08 13:48:23 +0530

    final changes

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -30,7 +30,7 @@ exports.resetPasswordToken = async(req,res) => {
         console.log("DETAILS", updatedDetails);
 
         //create url
-        const url = `http://localhost:3000/update-password/${token}`
+        const url = `https://edtech-study-notion-frontend-five.vercel.app/update-password/${token}`
         //send mail containing the url
         await mailSender(email,"Password reset link",
                             `Your Link for email verification is ${url}. Please click this url to reset your password.`);
